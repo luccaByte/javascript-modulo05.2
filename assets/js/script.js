716b82d9901f1f395caeee9e5c031cc3ead36d70
Lucas Gonçalves <103429796+Lucc-Minerva@users.noreply.github.com>
2023-10-26 16:03:45 +0000

    projeto finalizado

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,18 @@
+let produtoJs = document.querySelector ('.produto');
+
 let tituloJs = document.createElement ('h1');
 tituloJs.id = 'titulo';
+tituloJs.innerText = 'Loja Virtual';
+
+let cardOffer = document.createElement ('div');
+cardOffer.innerHTML = `
+    <h3>AMD Ryzen 5 5600G</h3>
+    <p>O Ryzen 5 5600G conta com 6 núcleos incríveis para quem quer apenas jogar. Os processadores AMD Ryzen série 5000 capacitam a próxima geração de jogos exigentes, proporcionando experiências imersivas únicas e dominando qualquer tarefa multithread como 3D e renderização de vídeo e compilação de software.</p>
+    <p>R$ 899.99</p>
+`;
+
+produtoJs.appendChild(tituloJs);
+produtoJs.appendChild(cardOffer);
+
+
+
